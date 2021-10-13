d11a2068d8215b425fbb1df35c61524bdd2483b5
wqc200 <wqc200@gmail.com>
2021-10-13 22:32:59 +0800

    add test_util

--- src/test/test_util.rs
+++ src/test/test_util.rs
@@ -1,40 +1,52 @@
-use std::sync::{Arc, Mutex};
-use crate::core::global_context::GlobalContext;
-use tokio::net::TcpListener;
-use crate::meta::{meta_util, initial};
-use crate::core::execution::Execution;
 use crate::config::def::MyConfig;
+use crate::core::execution::Execution;
+use crate::core::global_context::GlobalContext;
+use crate::meta::{initial, meta_util};
 use crate::mysql::error::MysqlResult;
+use std::sync::{Arc, Mutex};
+use tokio::net::TcpListener;
 
 pub async fn create_execution() -> MysqlResult<Execution> {
-    let global_context = Arc::new(Mutex::new(GlobalContext::new_with_config(MyConfig::default())));
+    let global_context = Arc::new(Mutex::new(GlobalContext::new_with_config(
+        MyConfig::default(),
+    )));
 
-    log4rs::init_file(global_context.lock().unwrap().my_config.server.log_file.to_string(), Default::default()).unwrap();
-
-    let addr = global_context.lock().unwrap().my_config.server.bind_host.to_string();
-    let listener = TcpListener::bind(&addr).await.unwrap();
-    log::info!("Listening on: {}", addr.clone());
+    log4rs::init_file(
+        global_context
+            .lock()
+            .unwrap()
+            .my_config
+            .server
+            .log_file
+            .to_string(),
+        Default::default(),
+    )
+    .unwrap();
 
     let result = meta_util::init_meta(global_context.clone()).await;
-    if let Err(e) = result {
-        log::error!("init meta error: {}", e);
-        return Err(e);
+    if let Err(mysql_error) = result {
+        log::error!("init meta error: {}", mysql_error);
+        return Err(mysql_error);
     }
 
     let result = meta_util::load_global_variable(global_context.clone());
-    if let Err(e) = result {
-        log::error!("load global variable error: {}", e);
-        return Err(e);
+    if let Err(mysql_error) = result {
+        log::error!("load global variable error: {}", mysql_error);
+        return Err(mysql_error);
     }
 
     let result = meta_util::read_all_schema(global_context.clone());
     match result {
         Ok(schema_map) => {
-            global_context.lock().unwrap().meta_data.add_all_schema(schema_map);
+            global_context
+                .lock()
+                .unwrap()
+                .meta_data
+                .add_all_schema(schema_map);
         }
-        Err(e) => {
-            log::error!("init meta schema error: {}", e);
-            return Err(e);
+        Err(mysql_error) => {
+            log::error!("init meta schema error: {}", mysql_error);
+            return Err(mysql_error);
         }
     }
 
@@ -42,29 +54,62 @@ pub async fn create_execution() -> MysqlResult<Execution> {
     let result = initial::read_all_table(global_context.clone());
     match result {
         Ok(table_def_map) => {
-            global_context.lock().unwrap().meta_data.add_all_table(table_def_map);
+            global_context
+                .lock()
+                .unwrap()
+                .meta_data
+                .add_all_table(table_def_map);
         }
-        Err(e) => {
-            log::error!("init meta table error: {}", e);
-            return Err(e);
+        Err(mysql_error) => {
+            log::error!("init meta table error: {}", mysql_error);
+            return Err(mysql_error);
         }
     }
 
-    let core_execution = Execution::new(global_context.clone());
+    let mut core_execution = Execution::new(global_context.clone());
+
+    let result = core_execution.try_init();
+    if let Err(mysql_error) = result {
+        return Err(mysql_error);
+    }
 
     Ok(core_execution)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::test::test_util::create_execution;
+    use crate::core::output::CoreOutput;
+    use crate::core::output::CoreOutput::FinalCount;
     use crate::mysql::error::MysqlResult;
+    use crate::mysql::{message, metadata};
+    use crate::test::test_util::create_execution;
+    use arrow::datatypes::{DataType, Field, Schema};
+    use arrow::record_batch::RecordBatch;
+    use datafusion::{assert_batches_eq, assert_batches_sorted_eq};
+    use std::sync::Arc;
 
     #[tokio::test]
-    async fn parallel_projection() -> MysqlResult<()> {
+    async fn show_databases() -> MysqlResult<()> {
         let mut core_execution = create_execution().await?;
 
-        let a = core_execution.execute_query("show databases").await?;
+        let result = core_execution.execute_query("show databases").await?;
+
+        let mut results: Vec<RecordBatch> = vec![];
+        match result {
+            CoreOutput::ResultSet(_, r) => results = r,
+            _ => {}
+        }
+
+        let expected = vec![
+            "+--------------------+",
+            "| Database           |",
+            "+--------------------+",
+            "| mysql              |",
+            "| performance_schema |",
+            "+--------------------+",
+        ];
+
+        assert_batches_eq!(expected, &results);
 
         Ok(())
     }
